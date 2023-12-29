f885411f0cbe657a11e4759194a2b2bf5646e580
Tahlil_Mahfuz <tahlilkfaiyaz@gmail.com>
2023-12-29 17:32:29 +0600

    added some files

--- realtime_detection.py
+++ realtime_detection.py
@@ -1,50 +1,37 @@
 import cv2
 from keras.models import model_from_json
-import numpy as np
+import numpy as numpy
 
-# Load model
 json_file = open("facialemotionmodel.json", "r")
 model_json = json_file.read()
 json_file.close()
 model = model_from_json(model_json)
-model.load_weights("facialemotionmodel.h5")
 
-# Load Haar Cascade
-haar_file = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
-face_cascade = cv2.CascadeClassifier(haar_file)
+model.load_weights("facialemotionmodel.h5")
+haar_file=cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+face_cascade=cv2.CascadeClassifier(haar_file)
 
 def extract_features(image):
     feature = np.array(image)
     feature = feature.reshape(1,48,48,1)
     return feature/255.0
 
-labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
-
-def start_realtime_emotion_detection():
-    webcam = cv2.VideoCapture(0)
-    while True:
-        ret, frame = webcam.read()
-        if not ret:
-            break
-
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_gray = cv2.resize(roi_gray, (48, 48))
-            img = extract_features(roi_gray)
+webcam=cv2.VideoCapture(0)
+labels = {0 : 'angry', 1 : 'disgust', 2 : 'fear', 3 : 'happy', 4 : 'neutral', 5 : 'sad', 6 : 'surprise'}
+while True:
+    i,im=webcam.read()
+    gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    faces=face_cascade.detectMultiScale(im,1.3,5)
+    try: 
+        for (p,q,r,s) in faces:
+            image = gray[q:q+s,p:p+r]
+            cv2.rectangle(im,(p,q),(p+r,q+s),(255,0,0),2)
+            image = cv2.resize(image,(48,48))
+            img = extract_features(image)
             pred = model.predict(img)
-            emotion = labels[np.argmax(pred)]
-            cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-
-        cv2.imshow("Output", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    webcam.release()
-    cv2.destroyAllWindows()
-
-# Example usage
-# start_realtime_emotion_detection()
+            prediction_label = labels[pred.argmax()]
+            cv2.putText(im, '% s' %(prediction_label), (p-10, q-10),cv2.FONT_HERSHEY_COMPLEX_SMALL,2, (0,0,255))
+        cv2.imshow("Output",im)
+        cv2.waitKey(27)
+    except cv2.error:
+        pass
